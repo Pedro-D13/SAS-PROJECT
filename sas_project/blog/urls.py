48f10e9d1eef59bfe319537e682021c4182bebd0
Pedro Dias <p.afonsosd1994@gmail.com>
2019-02-24 20:26:38 +0000

    added new class based view for the home page, using ListView for blog posts

--- sas_project/blog/urls.py
+++ sas_project/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from .views import PostListVIew
 
 from . import views
 
 app_name ="blog"
 urlpatterns = [
-    path('', views.home, name ='blog_home'),
+    path('', PostListVIew.as_view(), name ='blog_home'),
     path('about/', views.about, name='blog_about'),
     path('gallery/', views.gallery, name='blog_gallery'),
 ]
--- sas_project/blog/views.py
+++ sas_project/blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
+from django.views.generic import ListView
 from .models import Post
 # Create your views here.
 
-def home(request):
-    context = { 'posts':Post.objects.all()[:3:-1] }
-    return render(request, 'blog/home.html', context)
+class PostListVIew(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 def gallery(request):
     return render(request, 'blog/gallery.html')
