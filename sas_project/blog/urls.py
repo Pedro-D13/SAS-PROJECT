a38443cd33908d00f231e90c0753fcf5b8e48a20
Pedro Dias <p.afonsosd1994@gmail.com>
2019-02-24 22:19:34 +0000

    added more class based view, along with new templates and matching urls

--- sas_project/blog/urls.py
+++ sas_project/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import PostListVIew
+from .views import PostListView, PostDetailView , PostCreateView
 
 from . import views
 
 app_name ="blog"
 urlpatterns = [
-    path('', PostListVIew.as_view(), name ='blog_home'),
+    path('', PostListView.as_view(), name ='post_home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
+    path('post/new/<int:pk>/', PostCreateView.as_view(), name="post_create"),
     path('about/', views.about, name='blog_about'),
     path('gallery/', views.gallery, name='blog_gallery'),
 ]
--- sas_project/blog/views.py
+++ sas_project/blog/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView)
 from .models import Post
 # Create your views here.
 
-class PostListVIew(ListView):
+
+class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+
 def gallery(request):
     return render(request, 'blog/gallery.html')
+
 
 def about(request):
     context = {'posts': Post.objects.all()}
