b3459e9b53b4c779f84a88d92f5956cecbad9bad
Pedro Dias <p.afonsosd1994@gmail.com>
2019-02-25 08:14:34 +0000

    added ability to update and delete views, currently working

--- sas_project/blog/urls.py
+++ sas_project/blog/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
-from .views import PostListView, PostDetailView , PostCreateView
-
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostAllView,
+                    PostUpdateView,
+                    PostDeleteView)
 from . import views
 
-app_name ="blog"
+app_name = "blog"
 urlpatterns = [
-    path('', PostListView.as_view(), name ='post_home'),
+    path('', PostListView.as_view(), name='post_home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
-    path('post/new/<int:pk>/', PostCreateView.as_view(), name="post_create"),
+    path('post/new/', PostCreateView.as_view(), name="new"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post_update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post_delete"),
+    path('post/blog/', PostAllView.as_view(), name="post_all"),
     path('about/', views.about, name='blog_about'),
     path('gallery/', views.gallery, name='blog_gallery'),
 ]
--- sas_project/blog/views.py
+++ sas_project/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import (ListView,
                                   DetailView,
-                                  CreateView)
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 # Create your views here.
 
@@ -13,13 +16,54 @@ class PostListView(ListView):
     ordering = ['-date_posted']
 
 
+class PostAllView(ListView):
+    model = Post
+    template_name = "blog/blog.html"
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
 class PostDetailView(DetailView):
     model = Post
     context_object_name = 'post'
 
-class PostCreateView(CreateView):
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    context_object_name = 'post'
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def gallery(request):
