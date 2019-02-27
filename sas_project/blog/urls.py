2b0f8dde720075c8fb7ead4d6e60ffff7e66414c
Pedro Dias <p.afonsosd1994@gmail.com>
2019-02-27 07:48:17 +0000

    Pagination added to blog.html and user_html, users are able to see posts by a specifc user using pagination and filtering

--- sas_project/blog/urls.py
+++ sas_project/blog/urls.py
@@ -4,12 +4,14 @@ from .views import (PostListView,
                     PostCreateView,
                     PostAllView,
                     PostUpdateView,
-                    PostDeleteView)
+                    PostDeleteView,
+                    UserPostListView)
 from . import views
 
 app_name = "blog"
 urlpatterns = [
     path('', PostListView.as_view(), name='post_home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user_posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
     path('post/new/', PostCreateView.as_view(), name="post_new"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post_update"),
--- sas_project/blog/views.py
+++ sas_project/blog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView,
                                   UpdateView,
                                   DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from .models import Post
 # Create your views here.
 
@@ -14,19 +15,29 @@ class PostListView(ListView):
     template_name = "blog/home.html"
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User,username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostAllView(ListView):
     model = Post
     template_name = "blog/blog.html"
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 6
 
 
 class PostDetailView(DetailView):
     model = Post
     context_object_name = 'post'
-
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -36,7 +47,6 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
@@ -53,7 +63,6 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
-
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     context_object_name = 'post'
@@ -65,10 +74,8 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
-
 def gallery(request):
     return render(request, 'blog/gallery.html')
-
 
 def about(request):
     context = {'posts': Post.objects.all()}
