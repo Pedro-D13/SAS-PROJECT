ce3c232f108f0db64cd2fcc3a732218a0d764598
Pedro Dias <p.afonsosd1994@gmail.com>
2019-03-02 17:12:46 +0000

    reset password feature has been added, will send a password reset through gmail

--- sas_project/blog/views.py
+++ sas_project/blog/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.models import User
 from .models import Post
 # Create your views here.
 
-
 class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"
--- sas_project/users/models.py
+++ sas_project/users/models.py
@@ -4,20 +4,19 @@ from PIL import Image
 
 # Create your models here.
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete= models.CASCADE )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="member.png", upload_to="profile_pics")
 
     def __str__(self):
         return f"{self.user.username } Profile"
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
         if img.height > 200 or img.width > 200:
             output_size = (100, 100)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
-
